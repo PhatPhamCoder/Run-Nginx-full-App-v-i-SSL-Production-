113d9da178110c12a09b78b3d9404de45968e4e3
PhatPhamCoder <phamhoangminhphat@gmail.com>
2023-04-01 14:47:27 +0700

    first commit

---
+++ stackBE/src/services/tags.service.js
@@ -0,0 +1,6 @@
+const { TagsRepository } = require("../repositories");
+
+exports.retrieveAll = (result) => TagsRepository.retrieveAll(result);
+
+exports.retrieveOne = (tagName, result) =>
+  TagsRepository.retrieveOneWithCount(tagName, result);
